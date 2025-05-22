6e64a28cf0617a7f4b90504a84f8ed1da60f6e4f
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-05-22 12:53:54 +0200

    Create auth

--- Web-SOS_Code/Models/User.cs
+++ Web-SOS_Code/Models/User.cs
@@ -2,8 +2,7 @@
 {
     public class User
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }       
-        public string Password { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
---
+++ Web-SOS_Code/Services/AuthHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+
+namespace Web_SOS_Code.Services
+{
+    public class AuthenticationDelegatingHandler : DelegatingHandler
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AuthenticationDelegatingHandler(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = _httpContextAccessor.HttpContext.Request.Cookies["authToken"];
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
