ffb10ab76fd9f6829f3042f553484584f3fc0525
alvaro.cobo <alvaro.cobo.7e6@itb.cat>
2025-05-25 20:19:12 +0200

    Started ingredients

--- Web-SOS_Code/Models/Ingredient.cs
+++ Web-SOS_Code/Models/Ingredient.cs
@@ -2,6 +2,7 @@
 {
     public class Ingredient
     {
+        public int Id { get; set; }
         public required string Name { get; set; }
         public DateTime ExpirationDate { get; set; }
     }
---
+++ Web-SOS_Code/Pages/ListIngredient.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Web_SOS_Code.Models;
+using Web_SOS_Code.Services;
+
+namespace Web_SOS_Code.Pages;
+
+public class ListIngredientModel : PageModel
+{
+    private readonly IngredientService _ingredientService;
+
+    public ListIngredientModel(IngredientService ingredientService)
+    {
+        _ingredientService = ingredientService;
+    }
+
+    public List<Ingredient> Ingredients { get; set; } = new();
+
+    [TempData]
+    public string? ApiErrorMessage { get; set; }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        try
+        {
+            Ingredients = await _ingredientService.GetIngredientsAsync();
+        }
+        catch (HttpRequestException ex)
+        {
+            ApiErrorMessage = ex.Message;
+            ModelState.AddModelError(string.Empty, ApiErrorMessage);
+        }
+        catch (Exception ex)
+        {
+            ApiErrorMessage = $"Exception calling API: {ex.Message}";
+            ModelState.AddModelError(string.Empty, ApiErrorMessage);
+        }
+
+        return Page();  
+    }
+
+    public async Task<IActionResult> OnPostDeleteAsync(int id)
+    {
+        try
+        {
+            await _ingredientService.DeleteIngredientAsync(id);
+        }
+        catch (HttpRequestException ex)
+        {
+            ApiErrorMessage = ex.Message;
+            ModelState.AddModelError(string.Empty, ApiErrorMessage);
+        }
+        catch (Exception ex)
+        {
+            ApiErrorMessage = $"Exception calling API: {ex.Message}";
+            ModelState.AddModelError(string.Empty, ApiErrorMessage);
+        }
+        return RedirectToPage();
+    }
+}
